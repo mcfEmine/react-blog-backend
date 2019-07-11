fcf75b488a045d3971fc607eafc82e19961de30e
example <email@example.com>
2019-07-11 09:33:26 +0300

    db konfigürasyon

--- config/passport.js
+++ config/passport.js
@@ -7,15 +7,23 @@ const config = require('../config/db');
 module.exports = (passport) => {
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    console.log("***************************************************************************")
+    console.log("PASSPORT A GELDI.....");
+
     opts.secretOrKey = config.secret;
+
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        console.log("EXTRACT JWT, JWT_PAYLOAD_DATA" , jwt_payload.data._id);
+
             User.getUserById(jwt_payload.data._id, (err, user) => {
                 if (err) return done(err, false);
                 if (user) return done(null, user);
                 return done(null, false);
             });
+            console.log("PASSPORT USER BULDU " , user.body);
         
     }));
+    console.log("***************************************************************************")
 }
 
 
